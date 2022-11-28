60a79e3a6253f795bdb1478186c92dc188337406
uf-haadi-gill <89543816+uf-haadi-gill@users.noreply.github.com>
2022-11-28 16:05:58 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,10 @@
+//create main function
+
+//create function to parse move data
+//use two different data structures here to measure time difference, this is the main project 
+//potentially make different data structures depending on priority feature for optimization
+
+//create function to parse pokemon data
+//assign 'best' moves here by searching through the move data strucutres
+//make sure to measure time to have an efficiency measure
+
