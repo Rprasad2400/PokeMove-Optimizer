82f353e0ea7927ad3c44372c65eebcd8544fc5e7
gabjc <108655631+gabjc@users.noreply.github.com>
2022-11-29 15:22:27 -0500

    Update Pokemon.cpp

--- Pokemon.cpp
+++ Pokemon.cpp
@@ -1,1 +1,83 @@
+#include "Pokemon.h"
 
+    Pokemon::Pokemon(int _pokeDexNumber, std::string _name, std::string _type1, _std::string type2, int _total, 
+        int _hp, int _attack, int _defense, int _spAttack, int _spDef, int _speed, int _gen, bool _legendary)
+    {
+        pokeDexNumber = _pokeDexNumber
+        pokeName = _name;
+        pokeType1 = _type1;
+        pokeType2 = _type2;
+        total = _total;
+        hp = _hp;
+        attack = _attack;
+        defense = _defense;
+        spAttack = _spAttack;
+        spDef = _spDef;
+        speed = _speed;
+        gen = _gen;
+        legendary = _legendary;
+        pokemonMoves;
+    }
+
+    int Pokemon::getPokeDexNumber()
+    {
+        return pokeDexNumber;
+    }
+    std::string Pokemon::getName()
+    {
+        return pokeName;
+    }
+    std::string Pokemon::getType1();
+    {
+        return pokeType1;
+    }
+    std::string Pokemon::getType2()
+    {
+        return pokeType2;
+    }
+    int Pokemon::getTotal()
+    {
+        return total;
+    }
+    int Pokemon::getHP()
+    {
+        return hp;
+    }
+    int Pokemon::getAttack()
+    {
+        return attack;
+    }
+    int Pokemon::getDefense()
+    {
+        return defense;
+    }
+    int Pokemon::getSpAttack()
+    {
+        return spAttack;
+    }
+    int Pokemon::getSpDef()
+    {
+        return spDef;
+    }
+    int Pokemon::getSpeed()
+    {
+        return speed;
+    }
+    int Pokemon::getPokeGen()
+    {
+        return pokeGen;
+    }
+    int Pokemon::getLegendary()
+    {
+        return legendary;
+    }
+    std::vector<Move> getMoves()
+    {
+        return pokemonMoves;
+    }
+
+    void setMoves()
+    {
+        //search through the b-tree or hash-map, and then find which ever one has the highest power
+       //pokemonMoves.push_back( 
+    }
