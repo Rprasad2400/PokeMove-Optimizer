0d4cd66d83a79a11b151e39bbcc6617a6e4ce0f2
Rprasad2400 <38514904+Rprasad2400@users.noreply.github.com>
2022-12-05 17:57:35 -0500

    Update HashMap.h

--- HashMap.h
+++ HashMap.h
@@ -1,1 +1,41 @@
 
+#pragma once
+#include<vector>
+#include<iostream>
+#include<string>
+#include "Move.h";
+using namespace std;
+class HashMap {
+
+private:
+	class Node {
+	public:
+		Node() {
+		}
+		Node(Move move) {
+			val = move;
+			hasValue = true;
+
+		};
+		Node* next = nullptr;
+		Move val;
+		bool hasValue = false;
+		~Node() {
+			next = nullptr;
+		}
+	};
+	int numelements= 0;
+	int size = 10;
+	double loadFactor = 0.8;
+	
+	vector<Node> arrayList = vector<Node>(10);
+	void insertNode(Node* k, Move move);
+	int hashFunction(string name);
+	Move searchLinkedList(Node* k, string name);
+	bool removeNode(Node* k, string name);
+public:
+	Move get(string key);
+	void remove(string key);
+	void insert(Move value);
+
+};
