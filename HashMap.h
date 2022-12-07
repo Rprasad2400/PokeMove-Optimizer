3d695a70468a02b3f06179022ab6aa7ac22451cf
Rprasad2400 <38514904+Rprasad2400@users.noreply.github.com>
2022-12-06 23:34:45 -0500

    Update HashMap.h

--- HashMap.h
+++ HashMap.h
@@ -9,27 +9,38 @@ using namespace std;
 class HashMap {
 
 private:
+//Node used for the seperate chaining within the hashMap
 	class Node {
 	public:
+	//basic constructor
 		Node() {
 		}
+	//constructor used to add Move within the Node
 		Node(Move move) {
 			val = move;
 			hasValue = true;
 
 		};
+		//pointer to next Node
 		Node* next = nullptr;
+		//contains the value
 		Move val;
+		//checks whehther the Node has no value
 		bool hasValue = false;
 		~Node() {
 			next = nullptr;
 		}
 	};
+	//number of elements
 	int numelements= 0;
+	//current size of array
 	int size = 10;
+	//when to increase the size fo the array
 	double loadFactor = 0.8;
-	
+	//array used in the HashMap containing Nodes
 	vector<Node> arrayList = vector<Node>(10);
+
+	//helperFunctions
 	void insertNode(Node* k, Move move);
 	int hashFunction(string name);
 	Move searchLinkedList(Node* k, string name);
